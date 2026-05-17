f5d8f9022a7244ecae6c67b62fdd74cbff55285f
aditya-shekhar-git <adishekh831@gmail.com>
2026-05-17 11:50:12 +0530

    added Feed-page

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
+
+  const [feeds,setfeeds] = useState([])
+  const [loading,setLoading] = useState(true)
+
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostsData)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchFeeds()
+  }, [])
+  return !loading ?(
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+
+      {/*Stories and post list */}
+      <div>
+        <h1>Stories here</h1>
+        <div className='p-4 space-y-6'>
+          List of post
+        </div>
+      </div>
+      {/* Right Sidebar */}
+    </div>
+  ) : (
+    <Loading />
   )
 }
 
