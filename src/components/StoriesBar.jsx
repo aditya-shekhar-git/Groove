43377fe79d67b592411a7ff2d76d95ca546d5fef
aditya-shekhar-git <adishekh831@gmail.com>
2026-05-20 22:08:55 +0530

    StoryModal added

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { dummyStoriesData } from '../assets/assets'
 import { Plus } from 'lucide-react'
+import moment from 'moment'
+import StoryModal from './StoryModal'
 
 const StoriesBar = () => {
-
   const [stories, setStories] = useState([])
+  const [showModal, setShowModal] = useState(false)
+  const [viewStory, setViewStory] = useState(null)
 
   const fetchStories = async () => {
-    setStories(dummyPostsData)
+    setStories(dummyStoriesData)
   }
 
   useEffect(() => {
@@ -16,38 +19,70 @@ const StoriesBar = () => {
 
   return (
     <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scrollbar overflow-x-auto px-4'>
-
       <div className='flex gap-4 pb-5'>
-
+        
         {/* Create Story Card */}
-        <div className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-3/4 cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-linear-to-b from-indigo-50 to-white'>
-
+        <div onClick={() => setShowModal(true)} className='relative rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-3/4 cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-linear-to-b from-indigo-50 to-white'>
           <div className='h-full flex flex-col items-center justify-center p-4'>
-
             <div className='size-10 bg-indigo-500 rounded-full flex items-center justify-center mb-3'>
               <Plus className='w-5 h-5 text-white' />
             </div>
-
             <p className='text-sm font-medium text-slate-700 text-center'>
               Create Story
             </p>
-
           </div>
         </div>
 
         {/* Story Cards */}
+        {stories.map((story, index) => (
+          <div
+            key={index}
+            className='relative overflow-hidden rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-3/4 cursor-pointer hover:shadow-lg transition-all duration-200 border border-gray-200 bg-white'
+          >
+            {story.media_type === 'image' && (
+              <img
+                src={story.media_url}
+                alt='Story'
+                className='w-full h-full object-cover hover:scale-110 transition duration-500'
+              />
+            )}
 
-        {
-            stories.map((story,index) => (
-              <div key={index} className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-3/4 cursor-pointer hover:shadow-lg transition-all duration-200 border border-gray-200 bg-white'>
-                <img src={story.user.profile_picture} alt="Story" className='absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow' />
-                <p className='absolute top-18 left-3 text-white/60 text-sm truncate max-w-24'>{story.content}</p>
-                <p>{story.createdAt}</p>
+            {story.media_type === 'video' && (
+              <video
+                src={story.media_url}
+                className='w-full h-full object-cover hover:scale-110 transition duration-500'
+                muted
+                autoPlay
+                loop
+              />
+            )}
+
+            {story.media_type === 'text' && (
+              <div className='absolute inset-0 bg-black flex items-center justify-center p-4'>
+                <p className='text-white text-sm text-center'>
+                  {story.content}
+                </p>
               </div>
-            ))
-        }
+            )}
 
+            <img
+              src={story.user.profile_picture}
+              alt='User'
+              className='absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow'
+            />
+
+            <p className='absolute top-18 left-3 z-10 text-white/80 text-sm truncate max-w-24'>
+              {story.content}
+            </p>
+
+            <p className='absolute bottom-1 right-2 z-10 text-white text-xs'>
+              {moment(story.createdAt).fromNow()}
+            </p>
+          </div>
+        ))}
       </div>
+      {/* Story Modal */}
+      {showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories} />}
     </div>
   )
 }
