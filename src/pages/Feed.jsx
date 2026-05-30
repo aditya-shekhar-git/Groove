887b28c1e933f16c98f68686a25486eb81a4de54
aditya-shekhar-git <adishekh831@gmail.com>
2026-05-30 18:47:11 +0530

    Post List added

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,45 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
-import Loading from '../components/Loading'
-import StoriesBar from '../components/StoriesBar'
-import PostCard from '../components/PostCard'
+import React, { useEffect, useState } from 'react';
+import { dummyPostsData, assets } from '../assets/assets';
+import Loading from '../components/Loading';
+import StoriesBar from '../components/StoriesBar';
+import PostCard from '../components/PostCard';
 
 const Feed = () => {
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [feeds,setfeeds] = useState([])
-  const [loading,setLoading] = useState(true)
-
-  const fetchFeeds = async () => {
-    setfeeds(dummyPostsData)
-    setLoading(false)
-  }
+  const fetchFeeds = () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetchFeeds()
-  }, [])
-  return !loading ?(
-    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+    fetchFeeds();
+  }, []);
 
-     {/* Stories and post list */}
-   <div>
-    <StoriesBar />
-    <div className='p-4 space-y-6'>
-      {feeds.map((post) => (
-        <PostCard key={post._id} post={post} />
-       ))}
-    </div>
-   </div>
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and post list */}
+      <div>
+        <StoriesBar />
+        <div className='p-4 space-y-6'>
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
+      </div>
+
       {/* Right Sidebar */}
-       <div>
-        <div>
-          <h1>Sponserd</h1>
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className='w-72 rounded-md' alt="Sponsored" />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className='text-slate-400'>Boost your workflow using automated platform.</p>
         </div>
         <h1>Recent Messages</h1>
-       </div>
+      </div>
     </div>
   ) : (
     <Loading />
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
