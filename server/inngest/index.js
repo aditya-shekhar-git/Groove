6ec746950d0c063a3589f756fd410c1fd8517bc5
aditya-shekhar-git <adishekh831@gmail.com>
2026-06-06 17:41:16 +0530

    Inngest issue fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,67 +1,51 @@
 import { Inngest } from "inngest";
-
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Groove" });
-
 // Inngest function to store user data in database;
 const syncUserCreation = inngest.createFunction(
-  { id: 'sync-user-from-clerk' },
-  { event: 'clerk/user.created' },
-  async ({ event }) => {
-    const { id, email_addresses, first_name, last_name, image_url } = event.data;
-
-    let username = email_addresses[0].email_address.split('@')[0];
-
-    // Check availability of username
-    const user = await User.findOne({username})
-
-    if(user){
+  { id: 'sync-user-from-clerk', triggers: [{ event: 'clerk/user.created' }] }, 
+async ({ event }) => {
+const { id, email_addresses, first_name, last_name, image_url } = event.data;
+let username = email_addresses[0].email_address.split('@')[0];
+// Check availability of username
+const user = await User.findOne({username})
+if(user){
         username += Math.floor(Math.random()*10000)
     } 
-
-    const userData = {
+const userData = {
         _id:id,
         email:email_addresses[0].email_address,
         full_name:first_name + " " +last_name,
         profile_picture:image_url,
         username
     }
-    await User.findByIdAndUpdate(id,updateUserData)
+await User.create(userData)
   }
 );
-
 // Inngest Function to update user data
-
 const syncUserUpdation= inngest.createFunction(
-  { id: 'update-user-from-clerk' },
-  { event: 'clerk/user.updated' },
-  async ({ event }) => {
-    const { id, email_addresses, first_name, last_name, image_url } = event.data;
-
-    const username = email_addresses[0].email_address.split('@')[0];
-
-    // Check availability of username
-    const user = await User.findOne({username})
-
-    const updateUserData = {
+  { id: 'update-user-from-clerk', triggers: [{ event: 'clerk/user.updated' }] }, 
+async ({ event }) => {
+const { id, email_addresses, first_name, last_name, image_url } = event.data;
+const username = email_addresses[0].email_address.split('@')[0];
+// Check availability of username
+const user = await User.findOne({username})
+const updateUserData = {
         _id:id,
-        email_address:email_addresses[0],
+        email:email_addresses[0].email_address,
         full_name:first_name + " " +last_name,
         profile_picture:image_url,
         username
     }
-    await User.create(userData)
+await User.findByIdAndUpdate(id, updateUserData)
   }
 );
-
 // Inngest function to delete user data in database;
 const syncUserDeletion = inngest.createFunction(
-  { id: 'delete-user-with-clerk' },
-  { event: 'clerk/user.deleted' },
-  async ({ event }) => {
-    const { id } = event.data;
-
-   await User.findByIdAndDelete(id)
+  { id: 'delete-user-with-clerk', triggers: [{ event: 'clerk/user.deleted' }] }, 
+async ({ event }) => {
+const { id } = event.data;
+await User.findByIdAndDelete(id)
+  }
 );
-
 export const functions = [syncUserCreation,syncUserUpdation,syncUserDeletion];
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import {inngest,functions} from './inngest/index.js'
+import {serve} from 'inngest/express'
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('server is running'));
-app.use('/api/inngest',server({client: inngest,functions}))
+app.use('/api/inngest',serve({client: inngest,functions}))
 
 const PORT = process.env.PORT || 4000;
 
