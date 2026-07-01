df7fc38463f5594065b545ce7e36dd3dc401798b
aditya-shekhar-git <adishekh831@gmail.com>
2026-07-01 22:46:27 +0530

    User Routes created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -135,6 +135,9 @@ export const followUsers = async (req,res) => {
 
         const toUser = await User.findById(id)
         toUser.followers.push(userId);
+        await toUser.save();
+
+        res.json({success:true,message:`Now you are ollowing ${user.username}`})
     
 
     } catch (error) {
@@ -151,20 +154,17 @@ export const unfollowUsers = async (req,res) => {
         const {id} = req.body;
 
         const user = await User.findById(userId);
+        user.following = user.following.filter(user => user !== id);
+        await user.save();
 
-        if(user.following.includes(id)){
-            return res.json({success:false,message:`You are already following ${user.username}`})
-        }
+        const toUser = await User.findById(id);
+        toUser.followers = toUser.followers.filter(user => user !==userId);
+        await user.save();
 
-        user.following.push(id);
-        await user.save()
-
-        const toUser = await User.findById(id)
-        toUser.followers.push(userId);
-    
+        res.json({success:false,message:`You are no longer following ${userId}`})
 
     } catch (error) {
         console.log(error);
         res.json({success:false,message:error.message})
-    }
+    }  
 }
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { discoverUsers, followUsers, getUserData, unfollowUsers, updateUserData } from '../controllers/userController.jsx';
+import { protect } from '../middlewares/auth.js';
+import { upload } from '../configs/multer.js';
+
+const userRouter = express.Router();
+
+userRouter.get('/data',protect, getUserData);
+
+userRouter.post('/update',upload.fields([{name : 'profile' , maxCount:1},{name : 'profile' , maxCount:1}]) , protect, updateUserData);
+
+
+userRouter.post('/discover',protect, discoverUsers);
+userRouter.post('/follow',protect, followUsers);
+userRouter.post('/unfollow',protect, unfollowUsers);
+
+
+export default userRouter;
