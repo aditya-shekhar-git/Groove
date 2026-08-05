5ccd6ffd8d12c42ea806b5711552d37047941524
aditya-shekhar-git <adishekh831@gmail.com>
2026-08-05 08:10:40 +0530

    Implement Accept Connection Request

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -247,3 +247,31 @@ export const getUserConnections = async (req,res) => {
         res.json({success:false,message:error.message})
     }
 }
+
+//Accept Connection Request 
+export const acceptConnectionRequest = async (req,res) => {
+    try {
+
+        const {userId} = req.auth();
+        const {id} = req.body;
+
+        const connection = await Connection.findOne({from_user_id: id, to_user_id: userId})
+
+        if(!connection){
+            return res.json({success:false ,message: 'Connection not found'})
+        }
+
+        const user = await User.findById(userId)
+        user.connections.push(id);
+        await user.save();
+
+        connection.status = 'accepted'
+        await connection.save()
+
+        res.json({success:true, message:'Connection accepted successfully'})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
