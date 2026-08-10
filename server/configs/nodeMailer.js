0cff2cffa0b7467b6b930f99d2635e0758ab5710
aditya-shekhar-git <adishekh831@gmail.com>
2026-08-10 11:16:12 +0530

    email reminder added

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -3,17 +3,17 @@ import nodemailer from 'nodemailer'
 //Create a transporter object using the SMTP settings
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
+  host: "smtp-relay.brevo.com ",
   port: 587,
   auth: {
-    user: "",
-    pass: "",
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
 const sendEmail = async ({to,subject,body}) => {
     const response = await transporter.sendMail({
-        from: '',
+        from: process.env.SENDER_EMAIL,
         to,
         subject,
         html: body,
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,8 @@
-import { Inngest } from "inngest";
+import { eventType, Inngest } from "inngest";
 import connectDB from "../configs/db.js";
 import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 export const inngest = new Inngest({
   id: "Groove",
@@ -111,9 +113,117 @@ const syncUserDeletion = inngest.createFunction(
   }
 );
 
+//Inngest function to send reminder whne a new connection in added
+const sendConnectionRequestReminder = inngest.createFunction(
+  { id: "send-new-connection-request-reminder" },
+  { event: "app/connection-request" },
+  async ({ event, step }) => {
+    const { connectionId } = event.data;
+
+    const { connection, subject, body } = await step.run(
+      "send-connection-request-mail",
+      async () => {
+        const connection = await Connection.findById(connectionId).populate(
+          "from_user_id to_user_id"
+        );
+
+        const subject = "👋 New Connection Request";
+
+        const body = `
+          <div style="font-family: Arial, sans-serif; padding: 20px;">
+            <h2>Hi ${connection.to_user_id.full_name},</h2>
+
+            <p>
+              You have a new connection request from
+              ${connection.from_user_id.full_name} -
+              @${connection.from_user_id.username}
+            </p>
+
+            <p>
+              Click
+              <a
+                href="${process.env.FRONTEND_URL}/connections"
+                style="color: #10b981;"
+              >
+                here
+              </a>
+              to accept or reject the request
+            </p>
+
+            <br />
+
+            <p>Thanks,<br />PingUp - Stay Connected</p>
+          </div>
+        `;
+
+        return { connection, subject, body };
+      }
+    );
+
+    await sendEmail({
+      to: connection.to_user_id.email,
+      subject,
+      body,
+    });
+
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+
+    await step.run("send-connection-request-reminder", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id"
+      );
+
+      if (connection.status == "accepted") {
+        return { message: "Connection Already Accepted" };
+      }
+
+      const subject = "👋 New Connection Request";
+
+      const body = `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+          <h2>Hi ${connection.to_user_id.full_name},</h2>
+
+          <p>
+            You have a new connection request from
+            ${connection.from_user_id.full_name} -
+            @${connection.from_user_id.username}
+          </p>
+
+          <p>
+            Click
+            <a
+              href="${process.env.FRONTEND_URL}/connections"
+              style="color: #10b981;"
+            >
+              here
+            </a>
+            to accept or reject the request
+          </p>
+
+          <br />
+
+          <p>Thanks,<br />PingUp - Stay Connected</p>
+        </div>
+      `;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+
+      return {message:"Reminder Sent."}
+    });
+  }
+);
+
 export const functions = [
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
+  sendConnectionRequestReminder 
+
 ];
 
